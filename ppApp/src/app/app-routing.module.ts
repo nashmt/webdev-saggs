573a7663e9e8b8f3fd43ebaba6d647101acd6dc7
Matt Nash <Matt.Nash@softwareaggov.com>
2019-07-02 13:39:04 -0400

    added login/register files for demo

--- ppApp/src/app/app-routing.module.ts
+++ ppApp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent }  from './home/home.component';
+import { LoginComponent } from './login';
+import { DashboardComponent } from './dashboard';
 
-const routes: Routes = [];
+import { AuthGuard } from './_helpers';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
