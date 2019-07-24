c1ffa642f3c4641777a8de499389d0cc8bf30212
nashmt <nashmt515@gmail.com>
2019-07-24 11:52:31 -0400

    updated webpack

--- ppApp/webpack.config.js
+++ ppApp/webpack.config.js
@@ -1,17 +1,55 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV === 'development';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/main.ts',
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js','.scss','.jsx'],
         alias: {
             '@': path.resolve(__dirname, 'src/app/'),
         }
     },
     module: {
         rules: [
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                  isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      modules: true,
+                      localIdentName: '[name]__[local]___[hash:base64:5]',
+                      camelCase: true,
+                      sourceMap: isDevelopment
+                    }
+                  },
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: isDevelopment
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                  isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      sourceMap: isDevelopment
+                    }
+                  }
+                ]
+              },
+//----------------------------------
             {
                 test: /\.ts$/,
                 use: ['ts-loader', 'angular2-template-loader']
@@ -38,14 +76,18 @@ module.exports = {
             // global app config object
             config: JSON.stringify({
                 apiUrl: 'http://localhost:4000'
-            })
+            }),
         }),
 
         // workaround for warning: Critical dependency: the request of a dependency is an expression
         new webpack.ContextReplacementPlugin(
             /\@angular(\\|\/)core(\\|\/)fesm5/,
             path.resolve(__dirname, 'src')
-        )
+        ),
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+          })
     ],
     optimization: {
         splitChunks: {
